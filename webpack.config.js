8bf48320efee7a8952962b69815ef431da6a3d48
Shunsei Ishida <isd7835@gmail.com>
2019-04-16 10:42:14 +0900

    feat: Create an archetype of webpack.

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    'index': ['./src/index.js']
+  },
+  output: {
+    path: `${__dirname}/dist`
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    open: true,
+    openPage: 'index.html',
+    port: 8000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
